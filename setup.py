f37d98e957f1d83445dc10bb70f4ac87ddb78a3b
Lol4to <sidorov@satissoft.ru>
2015-12-11 15:05:38 +0300

    Generate setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='named-objects-extractor',
+    version='snapshot',
+    packages=['named_objects_extractor'],
+    url='https://github.com/Lol4t0/named-objects-extractor/',
+    license='MIT',
+    author='lol4t0',
+    author_email='sidorov.ij@gmail.com',
+    description='Extracting named entities: people, places, and organizations from text'
+)
